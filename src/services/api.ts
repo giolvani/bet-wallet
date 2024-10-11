350afb0879054843f6f8af710c7a715411a9c2c3
Giolvani <inavloig@gmail.com>
2024-10-11 17:50:58 -0300

    set up token with requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,17 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('wallet-token');
+    if (token && config.url !== '/login' && config.url !== '/register') {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
