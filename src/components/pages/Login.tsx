d75dd54dd1b4e96a8cc3b6a5bd3f0c709b615d20
Giolvani <inavloig@gmail.com>
2024-10-11 16:59:16 -0300

    create login page

---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+import { useAuth } from '@/contexts/AuthContext';
+
+const LoginPage = () => {
+    const { login } = useAuth();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        await login(email, password);
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginPage;
