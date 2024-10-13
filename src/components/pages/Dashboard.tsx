58d7ae91db6d0c2e8db0731ab2443ff374d0a1ac
Giolvani <inavloig@gmail.com>
2024-10-13 01:01:10 -0300

    create components to render dashboard

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,116 +1,24 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import api from '@/services/api';
 import { useAuth } from '@/contexts/AuthContext';
-
-interface Transaction {
-    id: string;
-    amount: number;
-    type: string;
-    createdAt: string;
-}
-
-interface Bet {
-    id: string;
-    amount: number;
-    status: string;
-    createdAt: string;
-    winAmount: number | null;
-}
+import Analytics from '@/components/partials/Analytics';
+import Header from '@/components/partials/header';
+import Bets from '@/components/partials/Bets';
+import Transactions from '@/components/partials/Transactions';
 
 const DashboardPage = () => {
-    const { balance, updateBalance } = useAuth();
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-    const [bets, setBets] = useState<Bet[]>([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [betAmount, setBetAmount] = useState<number>(0);
-    useEffect(() => {
-        fetchTransactions();
-        fetchBets();
-    }, []);
-
-    const fetchTransactions = async () => {
-        try {
-            const response = await api.get('/my-transactions?page=1&limit=10');
-            setTransactions(response.data.data);
-        } catch (error) {
-            console.error('Erro ao buscar as transações', error);
-            setErrorMessage('Erro ao buscar as transações.');
-        }
-    };
-
-    const fetchBets = async () => {
-        try {
-            const response = await api.get('/my-bets?page=1&limit=10');
-            setBets(response.data.data);
-        } catch (error) {
-            console.error('Erro ao buscar as apostas', error);
-            setErrorMessage('Erro ao buscar as apostas.');
-        }
-    };
-
-    const placeBet = async (betAmount: number) => {
-        try {
-            const response = await api.post('/bet', { amount: betAmount });
-            const { balance } = response.data;
-            updateBalance(balance);
-            fetchBets();
-            fetchTransactions();
-        } catch (error) {
-            console.error('Erro ao fazer a aposta', error);
-            alert('Erro ao fazer a aposta.');
-        }
-    };
-
-    const cancelBet = async (betID: string) => {
-        try {
-            await api.delete(`/my-bet/${betID}`);
-            fetchBets(); // Recarregar a lista de apostas após cancelamento
-        } catch (error) {
-            console.error('Erro ao cancelar a aposta', error);
-            setErrorMessage('Erro ao cancelar a aposta.');
-        }
-    };
+    const { balance } = useAuth();
 
     return (
-        <div>
-            <h1>Bem-vindo ao seu Dashboard</h1>
-            <h2>Saldo: R$ {balance.toFixed(2)}</h2>
+        <div className="container mx-auto py-8 space-y-6">
+            <Header />
+            <Analytics balance={balance} />
 
-            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-
-            <h3>Minhas Apostas</h3>
-            <input type="number" value={betAmount} onChange={(e) => setBetAmount(Number(e.target.value))} />
-            <button onClick={() => placeBet(betAmount)}>Fazer Aposta</button>
-
-            {bets?.length > 0 ? (
-                <ul>
-                    {bets.map((bet) => (
-                        <li key={bet.id}>
-                            {bet.createdAt} - Valor: R$ {bet.amount.toFixed(2)} - Status: {bet.status}
-                            {bet.winAmount && <span> - Prêmio: R$ {bet.winAmount.toFixed(2)}</span>}
-                            <button onClick={() => cancelBet(bet.id)}>Cancelar Aposta</button>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Nenhuma aposta encontrada.</p>
-            )}
-
-            <h3>Transações</h3>
-            {transactions?.length > 0 ? (
-                <ul>
-                    {transactions.map((transaction) => (
-                        <li key={transaction.id}>
-                            {transaction.createdAt} - {transaction.type} - R$ {transaction.amount.toFixed(2)}
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Nenhuma transação encontrada.</p>
-            )}
-        </div>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
+                <Bets />
+                <Transactions />
+            </div>
+        </div >
     );
 };
 
---
+++ src/components/partials/Bets.tsx
@@ -0,0 +1,121 @@
+import { useState, useEffect, useMemo } from "react";
+import { format } from "date-fns";
+import { CircleBackslashIcon } from "@radix-ui/react-icons";
+import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
+// import { useAuth } from "@/contexts/AuthContext";
+import api from "@/services/api";
+
+interface Bet {
+    id: string;
+    amount: number;
+    status: string;
+    createdAt: string;
+    winAmount: number | null;
+}
+
+export default function Bets() {
+    const limit = 5;
+    // const { balance, updateBalance } = useAuth();
+    // const [betAmount, setBetAmount] = useState('');
+    const [data, setData] = useState<Bet[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [total, setTotal] = useState(1);
+    // const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        fetchBets();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentPage]);
+
+    const fetchBets = async () => {
+        try {
+            const response = await api.get(`/my-bets?page=${currentPage}&limit=${limit}`);
+            setData(response.data.data);
+            setTotal(response.data.total);
+            setCurrentPage(response.data.page);
+        } catch (error) {
+            console.error('Erro ao buscar as apostas', error);
+        }
+    };
+
+    const paginationMessage = useMemo(() => {
+        const startItem = (currentPage - 1) * 5 + 1;
+        const endItem = Math.min(startItem + 4, total);
+        return `Exibindo ${startItem} a ${endItem} de ${total} registros.`;
+    }, [currentPage, total]);
+
+    // const handlePlaceBet = async () => {
+    //     try {
+    //         const response = await api.post('/bet', { amount: betAmount });
+    //         const { balance: updatedBalance } = response.data;
+    //         updateBalance(updatedBalance);
+    //         fetchBets();
+    //         setBetAmount('');
+    //     } catch (error) {
+    //         console.error('Erro ao fazer a aposta', error);
+    //         setErrorMessage('Erro ao fazer a aposta.');
+    //     }
+    // };
+
+    const handleCancelBet = async (betId: string) => {
+        try {
+            await api.delete(`/my-bet/${betId}`);
+            fetchBets();
+        } catch (error) {
+            console.error('Erro ao cancelar a aposta', error);
+        }
+    };
+
+    return <Card className="flex flex-col col-span-1">
+        <CardHeader>
+            <div className="flex justify-between items-center">
+                <CardTitle>Apostas Recentes</CardTitle>
+                <Button size="sm" className="ml-4">
+                    Apostar
+                </Button>
+            </div>
+            <CardDescription>&nbsp;</CardDescription>
+        </CardHeader>
+
+        <CardContent className="flex flex-col flex-grow">
+            {data.length > 0 ? (
+                <>
+                    <div className="space-y-1 flex-grow">
+                        {data.map((bet) => (
+                            <div key={bet.id} className="flex items-center">
+                                <div className="space-y-1">
+                                    <div className="text-sm text-muted-foreground">
+                                        <span>{format(new Date(bet.createdAt), 'dd/MM/yyyy HH:mm:ss')}</span>
+                                        <span className="ml-4">
+                                            <Badge>{bet.status}</Badge>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="ml-auto text-sm">R$ {bet.amount.toFixed(2)}</div>
+                                <Button onClick={() => handleCancelBet(bet.id)} size="sm" variant="destructive" className="ml-4" disabled={bet.status === 'canceled'}>
+                                    <CircleBackslashIcon />
+                                </Button>
+                            </div>
+                        ))}
+                    </div>
+
+                    <Separator className="my-4" />
+                    <div className='mt-auto flex items-center justify-end space-x-2'>
+                        <div className="flex-1 text-sm text-muted-foreground">{paginationMessage}</div>
+                        <div className="flex items-center space-x-2">
+                            <Button variant="outline" size="sm" disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)}>Previous</Button>
+                            <Button variant="outline" size="sm" disabled={currentPage === Math.ceil(total / limit)} onClick={() => setCurrentPage(currentPage + 1)}>Next</Button>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div className="flex-grow space-y-8">
+                    <p className="text-sm">Nenhuma transação encontrada.</p>
+                </div>
+            )}
+        </CardContent>
+    </Card>
+}
