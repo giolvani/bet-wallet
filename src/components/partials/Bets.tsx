37e7e8a28aad4c490b3e02894f5e53a6ce2a54e1
Giolvani <inavloig@gmail.com>
2024-10-13 01:50:54 -0300

    update amount after cancel bet

--- src/components/partials/Bets.tsx
+++ src/components/partials/Bets.tsx
@@ -5,7 +5,9 @@ import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/com
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
-// import { useAuth } from "@/contexts/AuthContext";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogFooter } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useAuth } from "@/contexts/AuthContext";
 import api from "@/services/api";
 
 interface Bet {
@@ -18,12 +20,12 @@ interface Bet {
 
 export default function Bets() {
     const limit = 5;
-    // const { balance, updateBalance } = useAuth();
-    // const [betAmount, setBetAmount] = useState('');
+    const { updateBalance } = useAuth();
     const [data, setData] = useState<Bet[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [total, setTotal] = useState(1);
-    // const [errorMessage, setErrorMessage] = useState('');
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [betAmount, setBetAmount] = useState('');
 
     useEffect(() => {
         fetchBets();
@@ -47,75 +49,100 @@ export default function Bets() {
         return `Exibindo ${startItem} a ${endItem} de ${total} registros.`;
     }, [currentPage, total]);
 
-    // const handlePlaceBet = async () => {
-    //     try {
-    //         const response = await api.post('/bet', { amount: betAmount });
-    //         const { balance: updatedBalance } = response.data;
-    //         updateBalance(updatedBalance);
-    //         fetchBets();
-    //         setBetAmount('');
-    //     } catch (error) {
-    //         console.error('Erro ao fazer a aposta', error);
-    //         setErrorMessage('Erro ao fazer a aposta.');
-    //     }
-    // };
+    const handlePlaceBet = async () => {
+        try {
+            const response = await api.post('/bet', { amount: Number(betAmount) });
+            updateBalance(response.data.balance);
+            setIsDialogOpen(false);
+            fetchBets();
+        } catch (error) {
+            console.error('Erro ao fazer a aposta', error);
+        }
+    };
 
     const handleCancelBet = async (betId: string) => {
         try {
-            await api.delete(`/my-bet/${betId}`);
+            const response = await api.delete(`/my-bet/${betId}`);
+            updateBalance(response.data.balance);
             fetchBets();
         } catch (error) {
             console.error('Erro ao cancelar a aposta', error);
         }
     };
 
-    return <Card className="flex flex-col col-span-1">
-        <CardHeader>
-            <div className="flex justify-between items-center">
-                <CardTitle>Apostas Recentes</CardTitle>
-                <Button size="sm" className="ml-4">
-                    Apostar
-                </Button>
-            </div>
-            <CardDescription>&nbsp;</CardDescription>
-        </CardHeader>
+    return (
+        <>
+            <Card className="flex flex-col col-span-1">
+                <CardHeader>
+                    <div className="flex justify-between items-center">
+                        <CardTitle>Apostas Recentes</CardTitle>
 
-        <CardContent className="flex flex-col flex-grow">
-            {data.length > 0 ? (
-                <>
-                    <div className="space-y-1 flex-grow">
-                        {data.map((bet) => (
-                            <div key={bet.id} className="flex items-center">
-                                <div className="space-y-1">
-                                    <div className="text-sm text-muted-foreground">
-                                        <span>{format(new Date(bet.createdAt), 'dd/MM/yyyy HH:mm:ss')}</span>
-                                        <span className="ml-4">
-                                            <Badge>{bet.status}</Badge>
-                                        </span>
-                                    </div>
+                        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                            <DialogTrigger asChild>
+                                <Button size="sm" className="ml-4">Apostar</Button>
+                            </DialogTrigger>
+
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Fazer uma Aposta</DialogTitle>
+                                </DialogHeader>
+
+                                <div className="space-y-4">
+                                    <Input
+                                        type="number"
+                                        placeholder="Valor da aposta"
+                                        value={betAmount}
+                                        onChange={(e) => setBetAmount(e.target.value)}
+                                    />
                                 </div>
-                                <div className="ml-auto text-sm">R$ {bet.amount.toFixed(2)}</div>
-                                <Button onClick={() => handleCancelBet(bet.id)} size="sm" variant="destructive" className="ml-4" disabled={bet.status === 'canceled'}>
-                                    <CircleBackslashIcon />
-                                </Button>
-                            </div>
-                        ))}
-                    </div>
 
-                    <Separator className="my-4" />
-                    <div className='mt-auto flex items-center justify-end space-x-2'>
-                        <div className="flex-1 text-sm text-muted-foreground">{paginationMessage}</div>
-                        <div className="flex items-center space-x-2">
-                            <Button variant="outline" size="sm" disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)}>Previous</Button>
-                            <Button variant="outline" size="sm" disabled={currentPage === Math.ceil(total / limit)} onClick={() => setCurrentPage(currentPage + 1)}>Next</Button>
-                        </div>
+                                <DialogFooter>
+                                    <Button onClick={handlePlaceBet} disabled={!betAmount}>
+                                        Confirmar Aposta
+                                    </Button>
+                                </DialogFooter>
+                            </DialogContent>
+                        </Dialog>
                     </div>
-                </>
-            ) : (
-                <div className="flex-grow space-y-8">
-                    <p className="text-sm">Nenhuma transação encontrada.</p>
-                </div>
-            )}
-        </CardContent>
-    </Card>
+                    <CardDescription>&nbsp;</CardDescription>
+                </CardHeader>
+
+                <CardContent className="flex flex-col flex-grow">
+                    {data.length > 0 ? (
+                        <>
+                            <div className="space-y-1 flex-grow">
+                                {data.map((bet) => (
+                                    <div key={bet.id} className="flex items-center">
+                                        <div className="space-y-1">
+                                            <div className="text-sm text-muted-foreground">
+                                                <span>{format(new Date(bet.createdAt), 'dd/MM/yyyy HH:mm:ss')}</span>
+                                                <span className="ml-4">
+                                                    <Badge>{bet.status}</Badge>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className="ml-auto text-sm">R$ {Number(bet.amount).toFixed(2)}</div>
+                                        <Button onClick={() => handleCancelBet(bet.id)} size="sm" variant="destructive" className="ml-4" disabled={bet.status === 'canceled'}>
+                                            <CircleBackslashIcon />
+                                        </Button>
+                                    </div>
+                                ))}
+                            </div>
+
+                            <Separator className="my-4" />
+                            <div className='mt-auto flex items-center justify-end space-x-2'>
+                                <div className="flex-1 text-sm text-muted-foreground">{paginationMessage}</div>
+                                <div className="flex items-center space-x-2">
+                                    <Button variant="outline" size="sm" disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)}>Previous</Button>
+                                    <Button variant="outline" size="sm" disabled={currentPage === Math.ceil(total / limit)} onClick={() => setCurrentPage(currentPage + 1)}>Next</Button>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="mt-auto flex"><p className="text-sm">Nenhuma aposta encontrada.</p></div>
+                    )}
+                </CardContent>
+            </Card>
+        </>
+    );
 }
